6f5dd955fcdfdc1989abb0d9ab857a2a732d94fc
RAi <swad@ya.ru>
2017-10-07 00:51:41 +0300

    +1 к успехам
    	modified:   index.html
    	modified:   js/my.js

--- js/my.js
+++ js/my.js
@@ -1,26 +1,36 @@
 
 //const firsts = <h1>Hello!</h1>;
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Всем привет, я компонент App!
+      </div>
+    );
+  }
+});
+/* */
+//* */
 
-/*
-var q = React.createClass(
-	{
-		render: function () {
+var Qwerty = React.createClass({
+		render: function() {
 			return (
-				"dslfjl"
-				)
+				<div>hjkfhsdk</div>
+				);
 		}
-	}
+	}); 
 
-); */
 
-//function fr() {
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Qwerty />,
   document.getElementById('first')
 );
 
-//}
+ReactDOM.render(
+  <App />,
+  document.getElementById('first3')
+);
 
 $("#first2").html('Вроде арбайтен');
 console.log(React);
