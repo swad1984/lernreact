1b711ed7d0a04af2f537213cddd1373c86602709
RAi <swad@ya.ru>
2017-10-06 23:11:16 +0300

    Что то получилось...
    	modified:   index.html
    	new file:   js/EventEmitter.js
    	new file:   js/my.js

---
+++ js/my.js
@@ -0,0 +1,15 @@
+
+//const firsts = <h1>Hello!</h1>;
+
+function fr() {
+
+ReactDOM.render(
+    React.createElement('h1', null, 'Привет, Мир!'),
+    document.getElementById('first')
+);
+
+}
+
+$("#first2").html('Вроде арбайтен');
+console.log(React);
+console.log(ReactDOM);
