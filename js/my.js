21aaed5178e49360b0c762f4c0559a12dfb7994d
RAi <swad@ya.ru>
2017-10-08 01:57:12 +0300

    Добавление для дебага 	modified:   js/my.js

--- js/my.js
+++ js/my.js
@@ -11,7 +11,39 @@ var w = [
 	}
 
 ];
+/*
+var fordebug = React.createClass({
+  propTypes: {
+    // Вы можете указать, каким примитивом должно быть свойство
+    optionalArray: React.PropTypes.array,
+    optionalBool: React.PropTypes.bool,
+    optionalFunc: React.PropTypes.func,
+    optionalNumber: React.PropTypes.number,
+    optionalObject: React.PropTypes.object,
+    optionalString: React.PropTypes.string,
 
+    // Вы может указать, что свойство можеть быть одни из ...
+    optionalUnion: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number,
+      React.PropTypes.instanceOf(Message)
+    ]),
+
+    // Вы можете указать, конкретную структуру объекта свойства
+    optionalObjectWithShape: React.PropTypes.shape({
+      color: React.PropTypes.string,
+      fontSize: React.PropTypes.number
+    }),
+
+    // Вы можете указать, что свойство ОБЯЗАТЕЛЬНО
+    requiredFunc: React.PropTypes.func.isRequired,
+
+    // Если нужно указать, что свойство просто обязательно, и может быть любым примитивом
+    requiredAny: React.PropTypes.any.isRequired,
+
+  }
+});
+*/
 var Appdet = React.createClass(
 	{
 		render: function() {
@@ -28,7 +60,10 @@ var Appdet = React.createClass(
 );
 
 var App = React.createClass({
-  render: function() {
+	propTypes: {
+		data: React.PropTypes.array.isRequired
+	},
+	render: function() {
   		//console.log(this.props.data);
 		var date = this.props.data;
 		
